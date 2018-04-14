8c1c6f10cbe41fca6d44d5fed03660e33de4af66
dtong <dtong@DTong-Mac-Pro.local>
2018-04-14 17:40:47 -0400

    Preliminary update(2018-4-14)

--- GettingStarted/GettingStarted/ViewController.swift
+++ GettingStarted/GettingStarted/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
         view.backgroundColor = lightOn ? UIColor.black : UIColor.white;
         lightOn = !lightOn;
     }
+    
     @IBAction func button(_ sender: Any) {
         clicks += 1;
         button.setTitle(String(clicks), for: .normal);
@@ -24,14 +25,55 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var friends = ["Alice", "Bob", "Carla", "David", "Elizabeth", "Frederick", "Gabriela", "Hugo", "Isabella", "Jason", "Kelly", "Lawrence", "Molly", "Nathan", "Olivia", "Pedro", "Quien es esto", "Richard", "Sarah"]
+        print(friends.contains("Alice"));
+        print(friends.contains("Alex"));
+        friends += ["托马斯"];
+        
+        var peopleAges = [
+            "Alice": 3,
+            "Jeff": 3,
+            "Kelvin": 0
+        ];
+        for item in 1 ... 5 {
+            print(item)
+        }
+        
+        for _ in 1 ... 5 {
+            print("iteration")
+        }
+        
+        class Animal {
+            var hasFur: Bool;
+            var foots: Int;
+            
+            init (hasFurProperty: Bool, numFoots: Int) {
+                hasFur = hasFurProperty;
+                foots = numFoots;
+            }
+            
+            func makeSound () {
+                print("Down with Communism!")
+                
+            }
+        }
+        
+        class Cat: Animal {
+            override func makeSound() {
+                print("Down with Neo-Capitalism!")
+            }
+        }
+        
+        let animation = Animal(hasFurProperty: true, numFoots: 4);
+        animation.makeSound()
+        
+                // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -7,43 +7,81 @@ var str = "Hello, playground"
 var num = 70
 
 /*
-if num == 3 {
-    print("num is 3")
-} else if num == 5 {
-    print("num is 5")
-} else {
-    print("default case")
-}
+ * Saturday 2018/3/24: Convert if-else chain to switch statement
+    if num == 3 {
+        print("num is 3")
+    } else if num == 5 {
+        print("num is 5")
+    } else {
+        print("default case")
+    }
+ 
+    switch num {
+        case 3:
+            print("num is 3")
+        case 5:
+            print("num is 5")
+        case 60 ... 100:
+            print("Nice work!")
+        default:
+            print ("default case")
+    }
 */
 
+func squareRoot (num: Double) -> Double {
+    return sqrt(num);
+}
+print(squareRoot(num: 4))
 
- 
+struct animal {
+    let name: String;
+    let species: String;
+    var age: Float;
+}
+var animal1 = animal(name: "Hydrogen", species: "Gas", age: 13491823899);
+var animal2 = animal(name: "Lithium", species: "Alkali metals", age: 13491823898.99989284790213)
 
-switch num {
-    case 3:
-        print("num is 3")
-    case 5:
-        print("num is 5")
-    case 60 ... 100:
-        print("Nice work!")
-    default:
-        print ("default case")
+struct Course {
+    let name: String;
+    var members: Array<User>;
+    let owner: Instructor;
+}
+
+struct Profile {
+    var image: CVBuffer;
+    var links: Array<String>;
+    var friends: Array<User>;
+}
+
+struct User {
+    var classes: Array<Course>;
+    let name: String;
+    var age: Int;
+    var profile: Profile;
+}
+
+struct Instructor {
+    // let user: User()
+    
 }
 
 /*
- 1. Convert the following switch statement to if statements or anything else you can come up with
- 
- switch num {
- case 3:
- print("num is 3")
- case 5:
- print("num is 5")
- default:
- print ("default case")
- }
- 
- 2. Read chapter 1.6 of "App development with Swift"
- 
- 3. Bonus
- Add a single case in the switch statement in "1" that prints "Nice work!" each time the variable num has a value between 60 and 100.
+struct Student {
+    // let user: User
+    
+    var GPA: Double;
+    
+    init () {
+        
+    }
+    
+    func getGPA () -> Double {
+        return GPA;
+    }
+    
+    mutating func changeGPA (newGPA: Double) {
+        GPA = newGPA;
+    }
+}
+
  */
