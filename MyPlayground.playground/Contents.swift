b46532f5cdfb5209853cc47ef96d9dad57cb91a6
Tetrahedry <tetrahedry@gmail.com>
2018-04-14 20:43:30 -0400

    Update 2018-4-14

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,87 +1,32 @@
-//: Playground - noun: a place where people can play
+//
+//  MyPlayground
+//
+
+//
+//  ViewController.swift
+//  CollectionLecture
+//
 
 import UIKit
 
-var str = "Hello, playground"
-
-var num = 70
-
-/*
- * Saturday 2018/3/24: Convert if-else chain to switch statement
-    if num == 3 {
-        print("num is 3")
-    } else if num == 5 {
-        print("num is 5")
-    } else {
-        print("default case")
+class Animal {
+    var hasFur: Bool
+    var feet: Int = 5;
+    init (hasFurProperty: Bool, numberOfFeet: Int) {
+        hasFur = hasFurProperty;
+        feet = numberOfFeet;
     }
- 
-    switch num {
-        case 3:
-            print("num is 3")
-        case 5:
-            print("num is 5")
-        case 60 ... 100:
-            print("Nice work!")
-        default:
-            print ("default case")
-    }
-*/
-
-func squareRoot (num: Double) -> Double {
-    return sqrt(num);
-}
-print(squareRoot(num: 4))
-
-struct animal {
-    let name: String;
-    let species: String;
-    var age: Float;
-}
-var animal1 = animal(name: "Hydrogen", species: "Gas", age: 13491823899);
-var animal2 = animal(name: "Lithium", species: "Alkali metals", age: 13491823898.99989284790213)
-
-struct Course {
-    let name: String;
-    var members: Array<User>;
-    let owner: Instructor;
-}
-
-struct Profile {
-    var image: CVBuffer;
-    var links: Array<String>;
-    var friends: Array<User>;
-}
-
-struct User {
-    var classes: Array<Course>;
-    let name: String;
-    var age: Int;
-    var profile: Profile;
-}
-
-struct Instructor {
-    // let user: User()
-    
-}
-
-/*
-struct Student {
-    // let user: User
-    
-    var GPA: Double;
-    
-    init () {
+    func makeSound () {
         
     }
-    
-    func getGPA () -> Double {
-        return GPA;
-    }
-    
-    mutating func changeGPA (newGPA: Double) {
-        GPA = newGPA;
-    }
 }
 
- */
+// TODO: Override parent variables
+// TODO 2: Display an image in the image view.
+
+class Cat: Animal {
+    override var feet = 4;
+    override func makeSound() {
+        print("meow");
+    }
+}
